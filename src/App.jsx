73273390a49da4115923ffeda88d0159703fc5bf
GlennMeekers <glennmeekers@gmail.com>
2025-05-19 17:10:46 +0200

    New project data capture, modal component, projects state

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,17 @@ import NoProjectSelected from "./components/NoProjectSelected";
 function App() {
   const [projectState, setProjectState] = useState({
     selectedProjectId: undefined,
-    project: [],
+    projects: [],
   });
+
+  // console.log(projectState);
 
   let content;
 
   if (projectState.selectedProjectId === null) {
-    content = <NewProject />;
+    content = (
+      <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
+    );
   } else if (projectState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
   }
@@ -26,9 +30,32 @@ function App() {
     });
   }
 
+  function handleCancelAddProject() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+      };
+    });
+  }
+
+  function handleAddProject(projectData) {
+    setProjectState((prevState) => {
+      const newProject = { ...projectData, id: Math.random() };
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <Sidebar onStartAddProject={handleStartAddProject} />
+      <Sidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectState.projects}
+      />
       {content}
     </main>
   );
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,20 +1,27 @@
 import { PROJECTS } from "../data";
 import Button from "./Button";
 
-export default function Sidebar({ onStartAddProject }) {
+export default function Sidebar({ onStartAddProject, projects }) {
   return (
     <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
       <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
         Your Projects
       </h2>
-      <Button onClick={onStartAddProject}>+ Add Project</Button>
-      {/* {PROJECTS.length > 0 && (
-        <ul>
-          {PROJECTS.map((project, index) => (
-            <li key={index}>{project.project_name}</li>
+      <div>
+        <Button onClick={onStartAddProject}>+ Add Project</Button>
+      </div>
+
+      {projects.length > 0 && (
+        <ul className="mt-8">
+          {projects.map((project) => (
+            <li key={project.id}>
+              <button className="w-full text-left px-2 py-1 rounded-sm my-1 text-stone-400 hover:text-stone-200 hover:bg-stone-800">
+                {project.title}
+              </button>
+            </li>
           ))}
         </ul>
-      )} */}
+      )}
     </aside>
   );
 }
