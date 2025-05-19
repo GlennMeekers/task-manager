f135809fa94009648f8b7646e062aa8ebbc20f7f
GlennMeekers <glennmeekers@gmail.com>
2025-05-19 13:31:43 +0200

    reusable button component, no project fallback screen component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject";
 import Sidebar from "./components/Sidebar";
+import NoProjectSelected from "./components/NoProjectSelected";
 
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    project: [],
+  });
+
+  let content;
+
+  if (projectState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
+  function handleStartAddProject() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <Sidebar />
-      <NewProject />
+      <Sidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
